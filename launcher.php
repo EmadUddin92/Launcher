cda870909ac59d72ddb85691d9071136faa3ac53
Emad Uddin <emaduddin715@gmail.com>
2018-08-04 23:16:51 +0600

    background-image-function-updated

--- launcher.php
+++ launcher.php
@@ -9,7 +9,7 @@
 <body>
 <div class="fh5co-loader"></div>
 
-<aside id="fh5co-aside" role="sidebar" class="text-center" style="background-image: url(images/img_bg_1_gradient.jpg);">
+<aside id="fh5co-aside" role="sidebar" class="text-center home-side" style="background-image: url(images/img_bg_1_gradient.jpg);">
     <h1 id="fh5co-logo">
         <a href="<?php site_url();?>">
             <?php bloginfo();?>
